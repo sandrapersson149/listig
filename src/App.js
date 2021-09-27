c092f09f1ed96c5ef6e72a386babbd44335d139d
Sandra Persson <sandra.persson@student.kyh.se>
2021-09-27 12:47:28 +0200

    started styling landing page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import Landing from "./Components/Landing";
 import SignIn from "./Components/SignIn";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ListPage from "./Components/ListPage";
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Roboto', sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+`
 
 function App() {
   return (
     <div>
+      <GlobalStyle />
       <Router>
         <Switch>
           <Route exact path='/'>
--- src/Components/Landing/index.js
+++ src/Components/Landing/index.js
@@ -1,22 +1,28 @@
 import React from 'react'
-import { LandingContainer, BtnContainer } from './landingStyled';
+import { LandingContainer, BtnContainer, Title, CreateAccBtn, SignInBtn, GoogleSignInBtn } from './landingStyled';
 import { Link } from "react-router-dom";
+import { FcGoogle } from 'react-icons/fc';
+
 
 export const Landing = () => {
   return (
     <LandingContainer>
-      <h1>Listig</h1>
+      <Title>Listig</Title>
       <h3>Klimatsmarta Inköpslistor</h3>
 
       <BtnContainer>
-        <h3>Välkommen till Listig</h3>
-        <p>Få koll på din klimatpåverkan</p>
-        <button>Skapa Konto</button>
-        <p>Har du ett konto? <span><Link to="/signin">Logga in</Link></span></p>
-        <p>Eller</p>
-        <button>
-          <Link to="/signin">Sign in with Google</Link>
-        </button>
+        <h4>Välkommen till Listig</h4>
+        <h5>Få koll på din klimatpåverkan</h5>
+        <CreateAccBtn>Skapa Konto</CreateAccBtn>
+        <p>Har du ett konto?
+          <SignInBtn>
+            <Link to="/signin">Logga in</Link>
+          </SignInBtn>
+        </p>
+        <p>____________ Eller ____________</p>
+        <GoogleSignInBtn>
+          <Link to="/signin"><span><FcGoogle /></span>Sign in with Google</Link>
+        </GoogleSignInBtn>
       </BtnContainer>
 
     </LandingContainer>
